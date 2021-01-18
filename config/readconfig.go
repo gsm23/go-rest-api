4101dcae739d21ef338352307746b60c34413ac4
Gourishankar_Mohapat <Gourishankar_Mohapat@Dell.com>
2021-01-18 21:50:12 +0530

    test

--- config/readconfig.go
+++ config/readconfig.go
@@ -2,33 +2,43 @@ package config
 
 import (
 	"fmt"
-	"gopkg.in/yaml.v1"
-	"io/ioutil"
+	//	"gopkg.in/yaml.v1"
+	//"io/ioutil"
 	"path/filepath"
-  )
+)
 
-  type Config struct {
+type Config struct {
 	Description string
-	Fruits map[string][]string
-  }
-
-func readfile() {
-	fmt.Println("Inside Config Module....")
-	filename, _ := filepath.Abs("../config.yaml")
-  	yamlFile, err := ioutil.ReadFile(filename)
-
-  	if err != nil {
-    	panic(err)
-  	}
-
-  	var config Config
-
-  	err = yaml.Unmarshal(yamlFile, &config)
-  	if err != nil {
-    	panic(err)
-  	}
-
-  	fmt.Printf("Value: %#v\n", config.Description)
-  	fmt.Printf("Value: %#v\n", config.Fruits)
+	Fruits      map[string][]string
 }
+
+func Readfile() {
+	fmt.Println("Inside Config Module....")
+	filename, err := filepath.Rel("../")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(filename)
+	//	yamlFile, err := ioutil.ReadFile(filename)
+	//	var config Config
+	//	err = yaml.Unmarshal(yamlFile, &config)
+	//	if err != nil {
+	//		panic(err)
+	//	}
+
+	/*	if err != nil {
+		    	panic(err)
+		  	}
+
+		  	var config Config
+
+		  	err = yaml.Unmarshal(yamlFile, &config)
+		  	if err != nil {
+		    	panic(err)
+		  	}
+
+		  	fmt.Printf("Value: %#v\n", config.Description)
+		  	fmt.Printf("Value: %#v\n", config.Fruits)
+			}
+	*/
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/gsm23/go-rest-api
 
 go 1.15
 
-require github.com/stretchr/testify v1.7.0 // indirect
+require (
+	github.com/stretchr/testify v1.7.0 // indirect
+	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0
+)
