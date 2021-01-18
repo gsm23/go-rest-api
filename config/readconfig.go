5952f635325c74f48895a885f2aa1f713f0fe051
Gourishankar_Mohapat <Gourishankar_Mohapat@Dell.com>
2021-01-18 21:41:04 +0530

    changes

--- config/readconfig.go
+++ config/readconfig.go
@@ -1,5 +1,34 @@
 package config
 
-func Test() {
+import (
+	"fmt"
+	"gopkg.in/yaml.v1"
+	"io/ioutil"
+	"path/filepath"
+  )
+
+  type Config struct {
+	Description string
+	Fruits map[string][]string
+  }
+
+func readfile() {
 	fmt.Println("Inside Config Module....")
+	filename, _ := filepath.Abs("../config.yaml")
+  	yamlFile, err := ioutil.ReadFile(filename)
+
+  	if err != nil {
+    	panic(err)
+  	}
+
+  	var config Config
+
+  	err = yaml.Unmarshal(yamlFile, &config)
+  	if err != nil {
+    	panic(err)
+  	}
+
+  	fmt.Printf("Value: %#v\n", config.Description)
+  	fmt.Printf("Value: %#v\n", config.Fruits)
+}
 }
